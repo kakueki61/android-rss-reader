c7d4ec90ed412a1704a60d8ad345f01a214cd5f4
kakueki61 <t.kodama61@gmail.com>
2014-03-02 20:53:41 +0900

    create new files for viewpager

---
+++ src/org/kakueki61/KatayamaProject/adapter/MainListFragment.java
@@ -0,0 +1,13 @@
+package org.kakueki61.KatayamaProject.adapter;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author <a href="mailto:">TakuyaKodama</a> (kodama-t)
+ * @version 1.00 14/03/02 kodama-t
+ */
+public class MainListFragment extends ListFragment {
+
+}
---
+++ src/org/kakueki61/KatayamaProject/adapter/MainViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package org.kakueki61.KatayamaProject.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author <a href="mailto:">TakuyaKodama</a> (kodama-t)
+ * @version 1.00 14/03/02 kodama-t
+ */
+public class MainViewPagerAdapter extends FragmentPagerAdapter {
+
+    public MainViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+
+        fragment = new MainListFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
